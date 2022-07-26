841fa864f2727f6a405300dafc26b2d22f3fa456
Yashsaraf96 <sarafyash2015@gmail.com>
2022-07-26 15:08:03 +0530

    Base homepage setup

--- app.js
+++ app.js
@@ -26,7 +26,9 @@ app.use(express.static("public"));
 
 
 
-
+app.get("/", function(req, res){
+  res.render("home",{homeStartingContent:homeStartingContent});
+});
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
